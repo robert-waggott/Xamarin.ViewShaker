26af6da15c01f5e2ee6e3bbbf07236c2497c9862
robert.waggott <rob@earthware.co.uk>
2014-06-12 18:11:10 +0100

    initial commit

---
+++ ViewShaker/ViewShaker.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using MonoTouch.UIKit;
+using MonoTouch.CoreAnimation;
+using MonoTouch.Foundation;
+
+namespace ViewShaker
+{
+	public class ViewShaker : CAAnimationDelegate
+	{
+		public event EventHandler<EventArgs> AnimationCompleted;
+
+		private const string ANIMATION_KEY = "kAFViewShakerAnimationKey";
+
+		private readonly IList<UIView> views;
+
+		private int completedAnimations = 0;
+
+		public ViewShaker(UIView view)
+			: this(new List<UIView> { view })
+		{
+		}
+
+		public ViewShaker(IList<UIView> views)
+		{
+			this.views = views;
+		}
+
+		public void Shake(double duration = 0.5)
+		{
+			var animation = CAKeyFrameAnimation.GetFromKeyPath("transform.translation.x");
+
+			animation.Delegate = this;
+			animation.Duration = duration;
+			animation.Values = new NSObject[] 
+			{  
+				NSNumber.FromFloat(0),
+				NSNumber.FromFloat(10),
+				NSNumber.FromFloat(-8),
+				NSNumber.FromFloat(8),
+				NSNumber.FromFloat(-5),
+				NSNumber.FromFloat(5),
+				NSNumber.FromFloat(0)
+			};
+			animation.KeyTimes = new NSNumber[] 
+			{ 
+				new NSNumber(0),
+				new NSNumber(0.225),
+				new NSNumber(0.425),
+				new NSNumber(0.6),
+				new NSNumber(0.75),
+				new NSNumber(0.875),
+				new NSNumber(1)
+			};
+			animation.TimingFunction = CAMediaTimingFunction.FromName(CAMediaTimingFunction.EaseInEaseOut);
+
+			foreach (var view in this.views)
+			{
+				view.Layer.AddAnimation(animation, ANIMATION_KEY);
+			}
+		}
+
+		public override void AnimationStopped(CAAnimation anim, bool finished)
+		{
+			completedAnimations += 1;
+
+			if (completedAnimations == this.views.Count)
+			{
+				this.completedAnimations = 0;
+
+				if (this.AnimationCompleted != null)
+				{
+					this.AnimationCompleted(this, EventArgs.Empty);
+				}
+			}
+		}
+	}
+}
