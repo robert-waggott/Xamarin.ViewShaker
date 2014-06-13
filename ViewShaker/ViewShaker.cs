6e68f6cff0d3fbf38e798c271e0379d0eac099c6
robert.waggott <rob@earthware.co.uk>
2014-06-13 14:00:32 +0100

    Organising using statements

--- ViewShaker/ViewShaker.cs
+++ ViewShaker/ViewShaker.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using MonoTouch.UIKit;
+
 using MonoTouch.CoreAnimation;
 using MonoTouch.Foundation;
+using MonoTouch.UIKit;
 
 namespace ViewShaker
 {
